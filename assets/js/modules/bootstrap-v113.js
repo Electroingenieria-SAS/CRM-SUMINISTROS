3edce7852c27bc216c94f3cdc94665b5dd918a4e
JEPTAC <j.perez@ei.com.co>
2026-09-01 14:41:28 -0500

    V11.3: activar segmentación visual de Jornada

--- assets/js/modules/bootstrap-v113.js
+++ assets/js/modules/bootstrap-v113.js
@@ -1,6 +1,7 @@
 import {state} from "../core/state.js";
 import {installGuidesV113,openModuleGuide} from "./guides-v113.js";
 import {installActivityBrowserV113} from "./activity-browser-v113.js";
+import {installWorkforceTaxonomyV113} from "./workforce-taxonomy-v113.js";
 
 installGuidesV113();
 
@@ -12,18 +13,19 @@ document.addEventListener("click",event=>{
   openModuleGuide(state.currentModule||"dashboard");
 },true);
 
-const startActivityBrowser=()=>{
+const startEnhancers=()=>{
   if(document.querySelector("#page-content")){
     installActivityBrowserV113();
+    installWorkforceTaxonomyV113();
     return true;
   }
   return false;
 };
 
-if(!startActivityBrowser()){
+if(!startEnhancers()){
   const app=document.querySelector("#app");
   if(app){
-    const observer=new MutationObserver(()=>{if(startActivityBrowser())observer.disconnect()});
+    const observer=new MutationObserver(()=>{if(startEnhancers())observer.disconnect()});
     observer.observe(app,{childList:true,subtree:true});
   }
 }
