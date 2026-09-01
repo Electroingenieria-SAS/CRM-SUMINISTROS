5a8d0eada9ef2ede90dbd47f67045609686bda6f
JEPTAC <j.perez@ei.com.co>
2026-09-01 15:25:41 -0500

    V11.5: activar guía específica de Recepción

--- assets/js/modules/bootstrap-v113.js
+++ assets/js/modules/bootstrap-v113.js
@@ -4,10 +4,12 @@ import {installActivityBrowserV113} from "./activity-browser-v113.js";
 import {installWorkforceTaxonomyV113} from "./workforce-taxonomy-v113.js";
 import {installAccessibilityV114} from "./accessibility-v114.js";
 import {installReceivingDomainV115} from "./receiving-hub-v115.js";
+import {installReceivingGuideV115} from "./receiving-guide-v115.js";
 
 installGuidesV113();
 installAccessibilityV114();
 installReceivingDomainV115();
+installReceivingGuideV115();
 
 document.addEventListener("click",event=>{
   const button=event.target.closest?.("[data-v113-guide-module]");
