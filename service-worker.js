92349fee4b5426b7c60aa49aa6fef10b47fcb48c
JEPTAC <j.perez@ei.com.co>
2026-09-04 10:29:54 -0500

    V11.8.2: cachear modulo renovado de auditoria

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE="crm-suministros-v11-8-1-20260904";
+const CACHE="crm-suministros-v11-8-2-20260904";
 const ASSETS=[
   "./","./index.html","./manifest.webmanifest","./templates/historical_orders.csv",
-  "./assets/css/app.css","./assets/css/operational-v112.css","./assets/css/guides-v113.css","./assets/css/workforce-taxonomy-v113.css","./assets/css/receiving-v115.css","./assets/css/receiving-guide-v115.css","./assets/css/inventory-scan-v116.css","./assets/css/v118.css",
+  "./assets/css/app.css","./assets/css/operational-v112.css","./assets/css/guides-v113.css","./assets/css/workforce-taxonomy-v113.css","./assets/css/receiving-v115.css","./assets/css/receiving-guide-v115.css","./assets/css/inventory-scan-v116.css","./assets/css/v118.css","./assets/css/audit-v1182.css",
   "./assets/img/logo-electroingenieria.png","./assets/img/iso-electroingenieria.png",
-  "./assets/js/main.js","./assets/js/config.js","./assets/js/core/icons.js","./assets/js/core/layout.js",
+  "./assets/js/main.js","./assets/js/config.js","./assets/js/core/icons.js","./assets/js/core/layout.js","./assets/js/core/format.js","./assets/js/core/ui.js",
   "./assets/js/modules/active-work.js","./assets/js/modules/work-clock.js","./assets/js/modules/workforce.js",
   "./assets/js/modules/operational-v112.js","./assets/js/modules/operational-resolve-guard-v112.js",
   "./assets/js/modules/guides-v113.js","./assets/js/modules/activity-browser-v113.js","./assets/js/modules/workforce-taxonomy-v113.js","./assets/js/modules/bootstrap-v113.js",
@@ -11,7 +11,7 @@ const ASSETS=[
   "./assets/js/modules/order-cancellation.js","./assets/js/modules/support-flow.js",
   "./assets/js/modules/receiving-order.js","./assets/js/modules/financial-flow.js","./assets/js/modules/picking-flow.js",
   "./assets/js/modules/cutting-flow.js","./assets/js/modules/shipping-flow.js","./assets/js/modules/queue.js",
-  "./assets/js/modules/orders.js","./assets/js/modules/inventory.js","./assets/js/services/api.js",
+  "./assets/js/modules/orders.js","./assets/js/modules/inventory.js","./assets/js/modules/audit.js","./assets/js/services/api.js",
   "./assets/js/services/supabase.js","./assets/js/services/materials.js","./assets/js/services/drive.js",
   "./assets/js/services/location.js","./assets/js/services/pdf-order-reader.js"
 ];
