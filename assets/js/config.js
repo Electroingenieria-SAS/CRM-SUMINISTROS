7375106856d073cd36bb41ca038429b48fb152ec
JEPTAC <j.perez@ei.com.co>
2026-09-04 11:08:32 -0500

    V11.8.5: actualizar versión del Centro de operaciones

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.8.4",
-  build: "2026-09-04.5",
+  version: "11.8.5",
+  build: "2026-09-04.6",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
