6d4574de19cf7d26c026ea5867fe3e3a868411aa
JEPTAC <j.perez@ei.com.co>
2026-09-04 16:14:24 -0500

    V11.10.0: actualizar versión de progreso global

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.9.9",
-  build: "2026-09-04.19",
+  version: "11.10.0",
+  build: "2026-09-04.20",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
