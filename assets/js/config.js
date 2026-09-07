df58e4a2987ede784de7f59b96609082897cd5fe
JEPTAC <j.perez@ei.com.co>
2026-09-07 10:30:15 -0500

    V11.11.0: actualizar versión final de Inventario

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.10.9",
-  build: "2026-09-07.03",
+  version: "11.11.0",
+  build: "2026-09-07.04",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
