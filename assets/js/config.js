560e3d10e61b9bc3917bc92752033ebeb41e3921
JEPTAC <j.perez@ei.com.co>
2026-09-07 15:53:38 -0500

    chore: identificar candidato visual V11.17.0

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.16.0",
-  build: "2026-09-07.10",
+  version: "11.17.0",
+  build: "2026-09-07.11",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
