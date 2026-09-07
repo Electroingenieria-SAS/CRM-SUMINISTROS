62e3515a40df6615a3a3cde35f6241bfbaa3f1f4
JEPTAC <j.perez@ei.com.co>
2026-09-07 11:05:29 -0500

    V11.12.0: actualizar versión por Flujo y tiempos

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.11.0",
-  build: "2026-09-07.04",
+  version: "11.12.0",
+  build: "2026-09-07.05",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
