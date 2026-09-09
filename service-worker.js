e54fcc503f69eb21bfa05572b5636a11c1c529de
JEPTAC <j.perez@ei.com.co>
2026-09-09 14:56:13 -0500

    V11.23.1 · rota caché PWA

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// previous-cache: crm-suministros-v11-21-1-20260908-12
-const CACHE="crm-suministros-v11-23-0-20260909-01";
+// previous-cache: crm-suministros-v11-23-0-20260909-01
+const CACHE="crm-suministros-v11-23-1-20260909-02";
 
 const APP_SHELL=[
   "./","./index.html","./404.html","./manifest.webmanifest","./templates/historical_orders.csv",
