a1cf1046f2ddd8300059889fb336919c2e435d7a
JEPTAC <j.perez@ei.com.co>
2026-09-18 14:03:35 -0500

    release: actualiza CONFIG a V11.31.1

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.31.0",
-  build: "2026-09-18.03",
+  version: "11.31.1",
+  build: "2026-09-18.04",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
