180b1efec03c0fa915b197d1b5b8540b7458ecbd
JEPTAC <j.perez@ei.com.co>
2026-09-23 11:12:45 -0500

    chore: eleva candidata a V11.34.3

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.34.2",
-  build: "2026-09-23.06",
+  version: "11.34.3",
+  build: "2026-09-23.07",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
