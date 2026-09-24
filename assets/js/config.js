42de8e758d216523dd746bf951d421d2fbb97b1e
JEPTAC <j.perez@ei.com.co>
2026-09-24 08:27:09 -0500

    release: bump CRM to v11.36.7

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = Object.freeze({
-  version: "11.36.6",
-  build: "2026-09-24.21",
+  version: "11.36.7",
+  build: "2026-09-24.22",
   appName: "CRM Suministros",
   company: "Electroingeniería S.A.S.",
   supabase: {
