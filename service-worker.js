ff7e683bdd3b8c8fa53d9209a82712c15ac156cd
JEPTAC <j.perez@ei.com.co>
2026-09-24 14:19:21 -0500

    release: rotate PWA cache for v11.38.1

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// previous-cache: crm-suministros-v11-37-4-20260924-27
-const CACHE="crm-suministros-v11-38-0-20260924-28";
+// previous-cache: crm-suministros-v11-38-0-20260924-28
+const CACHE="crm-suministros-v11-38-1-20260924-29";
 
 const APP_SHELL=[
   "./","./index.html","./404.html","./manifest.webmanifest","./templates/historical_orders.csv",
