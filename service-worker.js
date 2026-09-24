6d5d019197293bee88eade1988052be5e23384c5
JEPTAC <j.perez@ei.com.co>
2026-09-24 15:54:02 -0500

    V11.39.1 · renueva service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-// previous-cache: crm-suministros-v11-38-3-20260924-31
-const CACHE="crm-suministros-v11-39-0-20260924-32";
+// previous-cache: crm-suministros-v11-39-0-20260924-32
+const CACHE="crm-suministros-v11-39-1-20260924-33";
 
 const APP_SHELL=[
   "./","./index.html","./404.html","./manifest.webmanifest","./templates/historical_orders.csv",
